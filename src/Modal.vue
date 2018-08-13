279c4bb652ad56159cf301c4768b8384e5f06329
irzhakbasil <irzhakbasil@gmail.com>
2018-08-13 03:17:54 +0300

    css update

--- src/Modal.vue
+++ src/Modal.vue
@@ -1,7 +1,7 @@
 <template>
     <div class="container">
         <h3>New Truck Info</h3>
-            <div v-for="(value, key, index) in fields" :key='fields[index]'>
+            <div id="input-div" v-for="(value, key, index) in fields" :key='fields[index]'>
                 <input v-model = 'fields[key]' :placeholder = key>
             </div>
             <div>
@@ -60,6 +60,16 @@
         margin-top: 5px;
         margin-bottom: 10px;
     }
+    #input-div {
+        padding-bottom: 3px;
+    }
+    input {
+        text-indent: 10px
+    }
+    .container {
+        background-color: #c6dbec;
+    }
+    h3 {
+        color: #243447;
+    }
  </style>
-
- 
